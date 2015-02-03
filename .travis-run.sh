22e256ae5e100ffd6373502c5cbf25c6b88d8c6c
Guillaume Bour <guillaume@bour.cc>
2015-02-03 22:05:45 +0100

    [tests] update travis config to use wave.config file

--- .travis-run.sh
+++ .travis-run.sh
@@ -2,7 +2,7 @@
 # -*- coding: UTF8 -*-
 
 echo "** starting erlang wave server"
-erl -pa ebin/ `find deps -name ebin` -s wave_app -noinput &
+erl -pa ebin/ `find deps -name ebin` -s wave_app -config etc/wave.config -noinput &
 PID=$!
 echo " pid= $PID"
 sleep 10
