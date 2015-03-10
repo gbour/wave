7ee7661b3bec3108f08e5eb1b49347327b771ef0
Guillaume Bour <guillaume@bour.cc>
2015-03-10 10:38:21 +0100

    [tests] fix: travis-CI use ubuntu python2.7 version, without TLSv1.1 and TLSv1.2 support

--- tests/testsuite/003_ssl.py
+++ tests/testsuite/003_ssl.py
@@ -9,6 +9,9 @@ import ssl
 import os
 import os.path
 
+# TLS > v1 not available on python2.7 except for Debian
+SSL_VERSION = ssl.PROTOCOL_TLSv1_2 if hasattr(ssl, 'PROTOCOL_TLSv1_2') else ssl.PROTOCOL_TLSv1
+
 class Basic(TestSuite):
     def __init__(self):
         TestSuite.__init__(self, "SSL")
@@ -16,8 +19,9 @@ class Basic(TestSuite):
     @catch
     @desc("CONNECT")
     def test_01(self):
-        c = MqttClient("reg", ssl=True, ssl_opts={'ssl_version': ssl.PROTOCOL_TLSv1_2})
+        c = MqttClient("reg", ssl=True, ssl_opts={'ssl_version': SSL_VERSION})
         evt = c.do("connect")
+        print "Using SSL version:", c._c.sock.version()
 
         if not isinstance(evt, EventConnack):
             return False
@@ -28,7 +32,7 @@ class Basic(TestSuite):
     @desc("CONNECTION ERROR - FAILING certificate checking")
     def test_02(self):
         c = MqttClient("reg", ssl=True, ssl_opts={
-            'ssl_version': ssl.PROTOCOL_TLSv1_2,
+            'ssl_version': SSL_VERSION,
             'cert_reqs': ssl.CERT_REQUIRED
         })
         evt = c.do("connect")
@@ -42,7 +46,7 @@ class Basic(TestSuite):
     @desc("CONNECTION OK - SUCCESSFULL certificate checking")
     def test_03(self):
         c = MqttClient("reg", ssl=True, ssl_opts={
-            'ssl_version': ssl.PROTOCOL_TLSv1_2,
+            'ssl_version': SSL_VERSION,
             'cert_reqs': ssl.CERT_REQUIRED,
             'ca_certs': os.path.join(os.path.dirname(__file__), "../../", "etc/wave_cert.pem")
         })
@@ -57,7 +61,7 @@ class Basic(TestSuite):
     @desc("CONNECTION ERROR - invalid cipher")
     def test_04(self):
         c = MqttClient("reg", ssl=True, ssl_opts={
-            'ssl_version': ssl.PROTOCOL_TLSv1_2,
+            'ssl_version': SSL_VERSION,
             'cert_reqs': ssl.CERT_REQUIRED,
             'ca_certs': os.path.join(os.path.dirname(__file__), "../../", "etc/wave_cert.pem"),
             'ciphers': 'DES_CBC_SHA'
@@ -74,7 +78,7 @@ class Basic(TestSuite):
     @desc("CONNECTION OK - valid cipher")
     def test_05(self):
         c = MqttClient("reg", ssl=True, ssl_opts={
-            'ssl_version': ssl.PROTOCOL_TLSv1_2,
+            'ssl_version': SSL_VERSION,
             'cert_reqs': ssl.CERT_REQUIRED,
             'ca_certs': os.path.join(os.path.dirname(__file__), "../../", "etc/wave_cert.pem"),
             'ciphers': 'DHE-RSA-AES256-SHA256'
