fafc08a6778056410636045ed5fcaff161f22f57
Guillaume Bour <guillaume@bour.cc>
2015-11-09 22:25:48 +0100

    [mgmt] update travis config for rebar3

--- .travis-run.sh
+++ .travis-run.sh
@@ -2,7 +2,7 @@
 # -*- coding: UTF8 -*-
 
 echo "** starting erlang wave server"
-erl -pa ebin/ `find deps -name ebin` -s wave_app -config etc/wave.config -noinput &
+erl -pa `find _build -name ebin` -s wave_app -config etc/wave.config -noinput &
 PID=$!
 echo " pid= $PID"
 sleep 10
