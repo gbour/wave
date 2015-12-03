6fbb7e0f27149b73c8237e18946ce0d67063cd7a
Guillaume Bour <guillaume@bour.cc>
2015-12-03 16:31:55 +0100

    [test] update travis configuration to generate config from template

--- .travis-run.sh
+++ .travis-run.sh
@@ -2,7 +2,7 @@
 # -*- coding: UTF8 -*-
 
 echo "** starting erlang wave server"
-erl -pa `find _build -name ebin` -s wave_app -config etc/wave.config -noinput &
+erl -pa `find _build -name ebin` -s wave_app -config wave.travis.config -noinput &
 PID=$!
 echo " pid= $PID"
 sleep 10
