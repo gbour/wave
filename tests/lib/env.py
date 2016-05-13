a930eb2bb05e87ebec7a2ba906696227c184f866
Guillaume Bour <guillaume@bour.cc>
2016-05-13 13:28:02 +0200

    [feat] #24: prevent *zombie message workers* (by checking publisher/subscribers state regularly) NOTE: when a publisher/subscriber found dead, message is lost for this peer (we don't support persistant sessions in this case yet)

--- tests/lib/env.py
+++ tests/lib/env.py
@@ -2,9 +2,18 @@
 # -*- coding: utf8 -*-
 
 import random
+from twisted.internet import defer
 
 db = None
 twotp = None
 
 def gen_msg(msglen):
     return ''.join([chr(48+random.randint(0,42)) for x in xrange(msglen)])
+
+@defer.inlineCallbacks
+def remote(mod, fun, *args):
+    """ """
+    ret = yield twotp.callRemote('wave@127.0.0.1', mod, fun, *args)
+    defer.returnValue(ret)
+
+    
