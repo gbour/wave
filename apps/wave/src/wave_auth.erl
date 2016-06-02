ec32690bbae31d4e3c17d2b06c05f94f1c61312b
Guillaume Bour <guillaume@bour.cc>
2016-06-02 17:57:18 +0200

    [feat] #32: username/password are loaded at start in wave_auth ets, MQTT client username/password is validated against stored credentials (connection rejected if not match)

--- apps/wave/src/wave_auth.erl
+++ apps/wave/src/wave_auth.erl
@@ -16,29 +16,107 @@
 
 -module(wave_auth).
 -author("Guillaume Bour <guillaume@bour.cc>").
+-behaviour(gen_server).
 
 -export([check/4]).
+% gen_server API
+-export([start_link/1, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--spec check({ok, true|false}|undefined, DeviceID::binary(), Creds::{binary(), binary()},
+start_link(Args) ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, Args, []).
+
+init(Args) ->
+    ets:new(?MODULE, [set, named_table, private]),
+    erlang:send_after(500, self(), {reload, proplists:get_value(file, Args)}),
+    {ok, undefined}.
+
+%%
+%% PUBLIC API
+%%
+
+-spec check(true|false|undefined, DeviceID::binary(), Creds::{binary(), binary()},
             Setts::list({binary(), binary()})) -> {error, wrong_id|bad_credentials} | {ok, noauth|match}.
-check({ok, false}, _,_,_) ->
+check(false, _,{undefined, _}, _) ->
     lager:debug("no auth required"),
     {ok, noauth};
 
-check(_,DeviceID,_,[]) ->
-    lager:debug("unknown ~p DeviceID", [DeviceID]),
-    {error, wrong_id};
+% no username set
+check( true, _, {undefined, _}, _) ->
+    {error, bad_creds};
+%NOTE: this situation should never happen (see [MQTT-3.1.2-22])
+check(    _, _, {_, undefined}, _) ->
+    {error, bad_creds};
 
-check(_, DeviceID, Credentials, Settings) ->
-    lager:debug("auth check ~p (~p)", [DeviceID, Credentials]),
+check(    _, DeviceID, {User, Pwd}, Settings) ->
+    lager:debug("auth check ~p (~p)", [DeviceID, User]),
+    gen_server:call(?MODULE, {auth, DeviceID, User, Pwd}).
 
-    credential(Credentials, {
-        proplists:get_value(<<"username">>, Settings),
-        proplists:get_value(<<"password">>, Settings)
-    }).
+%%
+%% PRIVATE API
+%%
 
-credential(Credentials, Credentials) ->
-    {ok, match};
-credential(_,_) ->
-    {error, bad_credentials}.
+handle_call({auth, DeviceID, User, Password}, _, State) ->
+    Match = case ets:lookup(?MODULE, User) of
+        [{User, Hash}] ->
+            lager:debug("user ~p found", [User]),
+            case erlpass:match(Password, Hash) of
+                true -> {ok, match};
+                _    -> {error, bad_creds}
+            end;
+
+        _ -> 
+            % make it harder to guess if Username exists or not
+            % (as erlpass:match() takes around 600ms to execute)
+            erlpass:match(<<"foo">>,<<"$2a$12$6zOUIP0NEwBupO7ATO.Hv..ZQq5WGmyZ0rCYGUoznFrYpFZkr8ppy">>),
+            {error, bad_creds}
+    end,
+        
+    {reply, Match, State};
+
+handle_call(E,_,State) ->
+    lager:error("call ~p", [E]),
+    {reply, ok, State}.
+
+
+handle_cast(E, State) ->
+    lager:error("cast ~p", [E]),
+    {noreply, State}.
+
+handle_info({reload, File}, State) ->
+    ets:delete_all_objects(?MODULE),
+
+    % TODO: handle errors
+    {ok, F} = file:open(File, [read,binary]),
+    Count = fill_ets(F, file:read_line(F), 0),
+    file:close(F),
+
+    lager:debug("password file reloaded (~p lines found)", [Count]),
+    {noreply, State};
+
+handle_info(E, State) ->
+    lager:error("info ~p", [E]),
+    {noreply, State}.
+
+
+terminate(_,_) ->
+    ok.
+
+
+code_change(_, State, _) ->
+    {ok, State}.
+
+%%
+%% PRIVATE FUNS
+%%
+
+fill_ets(_, eof, Count) ->
+    Count;
+fill_ets(F, {ok, Line}, Count) ->
+    S = size(Line)-1, % Line includes trailing \n
+    <<Line2:S/binary, $\n>> = Line,
+
+    [User, Pwd] = binary:split(Line2, <<$:>>),
+    ets:insert(?MODULE, {User, Pwd}),
+
+    fill_ets(F, file:read_line(F), Count+1).
 
